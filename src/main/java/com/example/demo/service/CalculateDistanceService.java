b5a84984d47248a51e9ab5935591014687cb2a63
NatanVidori <natanvidorismo@hotmail.com>
2022-05-17 15:46:55 -0300

    Passa a realizar a conversão para o novo model. Passa a calcular a distancia entre cada rota e a distancia total.

--- src/main/java/com/example/demo/service/CalculateDistanceService.java
+++ src/main/java/com/example/demo/service/CalculateDistanceService.java
@@ -1,8 +1,13 @@
 package com.example.demo.service;
 
+import java.util.List;
+
 import com.example.demo.exceptions.ApiGoogleErrorException;
 import com.example.demo.gateway.feign.GoogleApi;
+import com.example.demo.gateway.model.factory.DistanceFactory;
 import com.example.demo.gateway.model.request.RouteRequest;
+import com.example.demo.gateway.model.response.Distance;
+import com.example.demo.gateway.model.response.DistanceRows;
 import com.example.demo.gateway.model.response.GetDistanceApi;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,15 +18,39 @@ public class CalculateDistanceService {
 
     @Autowired
     private GoogleApi googleApi;
-    
-    public GetDistanceApi execute(RouteRequest routeRequest) {
+
+    private Double totalDistance;
+
+    private Double totalDistanceRow;
+
+    public Distance execute(RouteRequest routeRequest) {
         try {
             GetDistanceApi getDistanceApi = googleApi.getDistance(routeRequest);
 
-            return getDistanceApi;
+            Distance distance = DistanceFactory.buildDistance(getDistanceApi);
+
+            distance.setTotalDistance(sumDistance(distance.getRows()));
+
+            return distance;
         } catch (Exception e) {
             throw new ApiGoogleErrorException();
         }
     }
 
+    private Double sumDistance(List<DistanceRows> rowsList) {
+        totalDistance = 0.00;
+
+        rowsList.forEach(row -> {
+            totalDistanceRow = 0.00;
+
+            row.getElements().forEach(elements -> {
+                totalDistance = totalDistance + elements.getDistance().getValue();
+                totalDistanceRow = totalDistanceRow + elements.getDistance().getValue();
+            });
+
+            row.setTotalDistanceRow(totalDistanceRow / 1000);
+        });
+
+        return totalDistance / 1000;
+    }
 }
